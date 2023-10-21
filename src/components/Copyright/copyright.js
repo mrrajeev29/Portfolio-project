0f029a96c5651184dc281b22e3a608a59369ab81
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2023-10-22 01:04:14 +0530

    Update copyright.js

--- src/components/Copyright/copyright.js
+++ src/components/Copyright/copyright.js
@@ -3,7 +3,7 @@ import './copyright.css'
 const Copyright=()=>{
     return(
         <div className="copy"><br/>
-        <h6 className="Raj">Copyright &copy; 2023 Aman Kr Mishra. All right reserved and created by <a className="Rajeev" href="https://portfolio-alpha-ruddy-27.vercel.app/" style={{textDecoration:"none"}}>Mr Rajeev Ranjan </a>.</h6>
+        <h6 className="Raj">Copyright &copy; 2023 Aman Kr Mishra. All right reserved and created by <a className="Rajeev" href="https://portfolio-alpha-ruddy-27.vercel.app/" style={{textDecoration:"none"}}>Mr Rajeev Ranjan </a> and Aman Kr Mishra.</h6>
         </div>
     )
 }
